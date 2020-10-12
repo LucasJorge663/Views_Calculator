fc52a2ed3e27c0f3dbf1f22b9b579216713cb732
LucasJorge663 <72530948+LucasJorge663@users.noreply.github.com>
2020-10-12 14:07:57 -0300

    Views Calculator
    Views calculator in java.

---
+++ src/exercicio1aula7_2bimestre/principal.java
@@ -0,0 +1,18 @@
+package exercicio1aula7_2bimestre;
+
+/**
+ *
+ * @author Lucas Jorge
+ */
+import views.tela1;
+
+public class principal {
+
+    public static void main(String[] args) {
+
+        tela1 telaSomar = new tela1();
+
+        telaSomar.setVisible(true);
+    }
+
+}
